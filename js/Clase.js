e06007cd9854fac8b1b4603fc4fc7659402b494c
rodrigo_oportus <roportus@cetep.cl>
2022-02-01 21:30:41 -0300

    Tarea funciones

---
+++ js/Clase.js
@@ -0,0 +1,127 @@
+/**
+ * Clase creada para el objeto clase
+ * Enfocada en clases para rutinas de ejercicios: Idea de proyecto final.
+ * IMPORTANTE: para inciar, digitar iniciarSimulador(); en consola del navegador.
+ */
+
+
+/**
+ * Clase "Clase"
+ */
+
+class Clase {
+
+    constructor(item) {
+        this.dia = item.dia;
+        this.horario = item.horario;
+        this.cantidadAlumnos = item.cantidadAlumnos;
+        this.tipoClase = item.tipoClase;
+    }
+
+    mostrarClase() {
+        console.log(`la clase tiene ${this.cantidadAlumnos} alumnos`);
+    }
+
+}
+
+
+/**
+ * Funciones para inicializar
+ */
+
+/*
+Globales:
+*/
+let iniciar;
+let diaSeleccionado;
+    let dia1 = "Lunes";
+    let dia2 = "Martes"; 
+let horario;
+    let horario1 = "10.00 Hrs.";
+    let horario2 = "13.00 Hrs.";
+let cantidadAlumnos;
+let tipoClase;
+    let tipo1 = "Funcional";
+    let tipo2 = "Crossfit";
+
+
+function iniciarSimulador() {
+    iniciar = "";
+    iniciar = prompt("¿Deseas crear una clase? \r\n \r\n Escribe 'SI' o 'NO'");
+    while (iniciar == "" || !iniciar) {
+        iniciar = prompt("Elige una alternativa correcta \r\n \r\n Escribe 'SI' o 'NO'");
+    }
+    if (iniciar.toUpperCase()=='NO'){
+        alert("No crearás una clase.");
+    } else if(iniciar.toUpperCase()=='SI'){
+        // Crear clase:
+        crearClase();
+    } else {
+        alert("Ingresa un valor correcto");
+        iniciarSimulador();
+    }
+}
+
+function crearClase() {
+
+    diaSeleccionado = parseInt(prompt(`¿Qué día se realizará la clase? \r\n \r\n Marca el número asociado: \r\n \r\n 1 - ${dia1} \r\n \r\n 2 - ${dia2} `));
+    while((diaSeleccionado=='' || !diaSeleccionado || isNaN(diaSeleccionado)) || diaSeleccionado>2) {
+        diaSeleccionado = parseInt(prompt(`PRECAUCIÓN, debes ingresar un valor correcto:\r\n \r\n 1 - ${dia1} \r\n \r\n 2 - ${dia2}`));
+    }
+    horario = parseInt(prompt(`¿Qué horario? \r\n \r\n Marca el número asociado: \r\n \r\n 1 - ${horario1} \r\n \r\n 2 - ${horario2}`));
+    while((horario=='' || !horario || isNaN(horario)) || horario>2) {
+        horario = parseInt(prompt(`PRECAUCIÓN, debes ingresar un valor correcto:\r\n \r\n 1 - ${horario1} \r\n \r\n 2 - ${horario2}`));
+    }
+    cantidadAlumnos = parseInt(prompt(`¿Cantidad de alumnos? \r\n \r\n Máximo 10:`));
+    while((cantidadAlumnos=='' || !cantidadAlumnos || isNaN(cantidadAlumnos)) || cantidadAlumnos>10) {
+        cantidadAlumnos = parseInt(prompt(`PRECAUCIÓN, debes ingresar un valor correcto: \r\n \r\n Máximo 10:`));
+    }
+    tipoClase = parseInt(prompt(`Tipo de clase? \r\n \r\n 1 - ${tipo1} \r\n \r\n 2 - ${tipo2}`));
+    while((tipoClase=='' || !tipoClase || isNaN(tipoClase)) || tipoClase>2) {
+        tipoClase = parseInt(prompt(`PRECAUCIÓN, debes ingresar un valor correcto: \r\n \r\n 1 - ${tipo1} \r\n \r\n 2 - ${tipo2}`));
+    }
+
+    ingresarClase();
+
+}
+
+
+function ingresarClase(){
+
+    const datosClase = {
+        dia: diaSeleccionado,
+        horario : horario,
+        cantidadAlumnos: cantidadAlumnos,
+        tipoClase: tipoClase
+    }
+
+    const claseIngresada = new Clase(datosClase);
+
+    if (claseIngresada) {
+        let generarInvitacion = parseInt(prompt(`Clase ingresada con éxito. ¿Deseas generar las invitaciones para los ${cantidadAlumnos} alumnos? \r\n \r\n 1 - SI \r\n \r\n 2 - NO  \r\n \r\n 2 - Resetear el generador`));
+        while (generarInvitacion == "" || !generarInvitacion || isNaN(generarInvitacion) || generarInvitacion>3) {
+            generarInvitacion = prompt("Elige una alternativa correcta \r\n \r\n 1 - SI \r\n \r\n 2 - NO  \r\n \r\n 3 - Resetear el generador.");
+        }
+        if (generarInvitacion==1){
+            alert("enviar invitaciones");
+            enviarInvitaciones();
+        } else if(generarInvitacion==2){
+            alert("No enviarás las invitaciones.");
+        } else {
+            alert("RESET");
+            iniciarSimulador();
+        } 
+    }
+    
+}
+
+function enviarInvitaciones (){
+
+        let diaClase = (diaSeleccionado==1) ? dia1 : dia2;
+        let horarioClase = (horario==1) ? horario1 : horario2;
+        let tipo = (tipoClase==1) ? tipo1 : tipo2;
+
+        for(let i = 1 ; i<=cantidadAlumnos; i++) {
+            console.log(`Invitación ${i} \r\n \r\n Día clase: ${diaClase} \r\n \r\n Horario: ${horarioClase} \r\n \r\n Tipo clase: ${tipo}`)
+        }
+}
--- js/script.js
+++ js/script.js
@@ -8,74 +8,8 @@ Sistema tickets JS
  */
 
 // Obtener año actual:
+
+
 const d = new Date();
 const YEAR = d.getFullYear();
 
-// Shows y tickets disponibles:
-let a = Math.floor(Math.random() * 10);
-let b = Math.floor(Math.random() * 10);
-let c = Math.floor(Math.random() * 10);
-let show = "";
-let tickets = "";
-const showGorillaz = `#1 | Gorillaz - Humanz tour reloaded: ${a} disponibles.`;
-const showDavidBowie = `#2 | David Bowie - Came back from the other side: ${b} disponibles`;
-const showSpinetta = `#3 |Spinetta: Post Crucifixión: ${c} disponibles`;
-
-/**
- * Comienzo del programa:
- */
-
-let user = prompt('Bienvenido al generador de tickets! Indique por favor su usuario de caja');
-// Validador nombre:
-while(user=='' || !user) {
-    user = prompt('Precaución: Debe ingresar su usuario:');
-}
-
-let pass = prompt('Ingresa tu password');
-// Validador password:
-while(pass=='' || !pass) {
-    pass = prompt('Precaución: Debe ingresar su password:');
-}
-
-let pass_check = prompt('Confirma tu password');
-// Validador check password:
-while((pass_check=='' || !pass_check) || (pass!=pass_check)) {
-    pass_check = prompt('Precaución: Password debe coincidir con la primera ingresada:');
-}
-
-let showEscogido = parseInt(prompt(`Bienvenido, ${user}, Por favor escoge el número show para el cual necesites imprimir ticket(s): \r\n \r\n ${showGorillaz} \r\n \r\n ${showDavidBowie} \r\n \r\n ${showSpinetta} `));
-// Validador show escogido:
-while((showEscogido=='' || !showEscogido || isNaN(showEscogido)) || (showEscogido<1 || showEscogido>3)) {
-    showEscogido = parseInt(prompt(`PRECAUCIÓN, Por favor escoge el número show para el cual necesites imprimir ticket(s): \r\n \r\n ${showGorillaz} \r\n \r\n ${showDavidBowie} \r\n \r\n ${showSpinetta} `));
-}
-
-
-if (showEscogido==1){
-    alert(`Show escogido: ${showGorillaz}`);
-    show = showGorillaz;
-    tickets = a;
-} else if (showEscogido==2){
-    alert(`Show escogido: ${showDavidBowie}`);
-    show = showDavidBowie;
-    tickets = b;
-} else if (showEscogido==3){
-    alert(`Show escogido: ${showSpinetta}`);
-    show = showSpinetta;
-    tickets = c;
-} else {
-    // Caso que no debiese ser ejecutado:
-    alert("No existe show indicado. Por favor avisar al encargado.");
-}
-
-let tickets_asignados = parseInt(prompt(`Existen ${tickets} tickets disponibles. ¿Cuántos deseas asignar?:`));
-
-
-while((tickets_asignados=='' || !tickets_asignados || isNaN(tickets_asignados)) || tickets_asignados>tickets) {
-    tickets_asignados = parseInt(prompt(`PRECAUCIÓN, para ${show} existen ${tickets} tickets.`));
-}
-
-alert(`Se generarán ${tickets_asignados} tickets para el show.`);
-
-for (let i = 1 ; i < tickets_asignados ; i++) {
-    alert(`Usuario creador: ${user} | año: ${YEAR} - Entrada #${i}`);
-}
